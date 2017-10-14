0c1cb2e7e23a2c84a1c5d3636b571f303bcb8769
Joe Liotta <joe.liotta00@gmail.com>
2017-10-14 13:45:11 -0700

    Disable other friend req button in notification entry when clicking the other

--- client/src/components/NotificationListEntry.js
+++ client/src/components/NotificationListEntry.js
@@ -4,9 +4,17 @@ import { Menu, Input, Button, Dropdown, Card } from 'semantic-ui-react';
 class NotificationListEntry extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      accepted: null,
-      declined: null
+  }
+
+  handleAcceptance() {
+    if (!this.props.declined) {
+      this.props.acceptFriendRequest(this.props.user, this.props.notification)
+    }
+  }
+
+  handleDecline() {
+    if (!this.props.accepted) {
+      this.props.handleDeclineClick(this.props.user, this.props.notification);
     }
   }
 
@@ -18,8 +26,8 @@ class NotificationListEntry extends Component {
         </Card.Content>
         <Card.Content>
           <Button.Group size="mini" attached="bottom">
-            <Button color={this.props.accepted ? 'teal' : 'green'} onClick={() => this.props.acceptFriendRequest(this.props.user, this.props.notification)}>{this.props.accepted ? 'Accepted!' : 'Accept'}</Button>
-            <Button color="red" onClick={() => this.props.handleDeclineClick(this.props.user, this.props.notification)}>{this.props.declined ? 'Declined!' : 'Decline'}</Button>
+            <Button diasbled={this.props.declined} color={this.props.accepted ? 'teal' : 'green'} onClick={() => this.handleAcceptance()}>{this.props.accepted ? 'Accepted!' : 'Accept'}</Button>
+            <Button disabled={this.props.accepted}color="red" onClick={() => this.handleDecline()}>{this.props.declined ? 'Declined!' : 'Decline'}</Button>
           </Button.Group>
         </Card.Content>
       </Card>
